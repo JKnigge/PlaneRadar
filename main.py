32eba9425898611e250f880f5ef001ea77f6ac29
jknigge <j-k.knigge@gmx.de>
2024-11-02 16:15:56 +0100

    Use Puma to Process File in Chunks

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import argparse
-import csv
 import datetime
 import math
 import os
@@ -15,6 +14,7 @@ from luma.oled.device import sh1106  # For real LCD screen
 from PIL import ImageFont
 from math import radians, sqrt, atan2, cos
 from PIL import ImageDraw, Image
+import pandas as pd
 
 from SBSMessage import SBSMessage
 from database_models import Callsigns, Positions
@@ -40,17 +40,25 @@ def make_font(name, size):
     return ImageFont.truetype(font_path, size)
 
 
-def read_aircraft_data(file_content):
-    return dict(
-        (
-            line["icao24"],
-            {
-                "registration": line["registration"],
-                "typecode": line["typecode"],
-                "operator": line["operatoricao"]
+def read_aircraft_data(local_file):
+    aircraft_data = {}
+
+    for chunk in pd.read_csv(local_file, chunksize=1000):
+        # Filter out rows where "icao24" is empty
+        chunk = chunk[chunk["icao24"].notna()]
+
+        # Convert each chunk to the desired dictionary structure and update the main dictionary
+        chunk_dict = {
+            row["icao24"]: {
+                "registration": row["registration"],
+                "typecode": row["typecode"],
+                "operator": row["operatoricao"]
             }
-        ) for line in csv.DictReader(file_content) if line["icao24"] != ""
-    )
+            for _, row in chunk.iterrows()
+        }
+        aircraft_data.update(chunk_dict)
+
+    return aircraft_data
 
 
 def get_aircraft_data(download_file: bool):
@@ -245,7 +253,7 @@ def update_screen(screentime_in_seconds: int, keepon: bool):
 
     if last_screen_update is None:
         last_screen_update = datetime.datetime.now()
-        display_closest_aircraft()
+        display_closest_aircraft(keepon)
         return
 
     time_now = datetime.datetime.now()
@@ -390,5 +398,4 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-
     main(args.download, args.screentime, args.keepon)
