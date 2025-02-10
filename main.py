f13184ebf8db113c08afb02154d3c4920edd6f8a
jknigge <j-k.knigge@gmx.de>
2025-02-11 00:27:41 +0100

    Fix Text Position

--- main.py
+++ main.py
@@ -339,7 +339,7 @@ def write_on_screen(callsign: Callsigns, position: Positions, keepon: bool):
     draw.text((5, 55), "\uf017", font=awesome_font_small, fill="white")
     if position.message_received is not None:
         message_timestamp = position.message_received.strftime("%H:%M:%S")
-        draw.text((10, 60), f"{message_timestamp} ({position.num_message})", font=font_normal, fill="white")
+        draw.text((15, 55), f"{message_timestamp} ({position.num_message})", font=font_normal, fill="white")
     draw_small_compass(draw, 110, 40, position.bearing)
 
     if low_alt_prio_switch_state == GPIO.LOW:
